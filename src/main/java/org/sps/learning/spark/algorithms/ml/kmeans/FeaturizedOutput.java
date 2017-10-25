2969ca4951788d13914612eb6cdc31dfd36ac102
Yasset Perez-Riverol <ypriverol@gmail.com>
2017-10-26 00:24:06 +0100

    best logging system has been added

---
+++ src/main/java/org/sps/learning/spark/algorithms/ml/kmeans/FeaturizedOutput.java
@@ -0,0 +1,17 @@
+package org.sps.learning.spark.algorithms.ml.kmeans;
+
+/**
+ * This code is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * ==Overview==
+ * <p>
+ * This class
+ * <p>
+ * Created by ypriverol (ypriverol@gmail.com) on 25/10/2017.
+ */
+public class FeaturizedOutput {
+}
